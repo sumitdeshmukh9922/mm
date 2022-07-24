e10e7e12fe483046bc5b7ec1c9237cc8eaee9330
sumitdeshmukh9922 <sumitdeshmukh9922@gmail.com>
2022-07-24 20:03:57 +0530

    first commit

---
+++ Server/app.js
@@ -0,0 +1,38 @@
+const dotenv = require('dotenv');
+const express = require('express');
+const app = express();
+
+dotenv.config({ path: './config.env' });
+require('./db/conn');
+
+const PORT = process.env.PORT;
+
+const middleware = (req, res, next) => {
+  console.log('This is middleware');
+  next();
+};
+
+app.get('/', (req, res) => {
+  res.send('Hello world from server');
+});
+
+app.get('/about', middleware, (req, res) => {
+  console.log('Middleware is working');
+  res.send('Hello world from about');
+});
+
+app.get('/contact', (req, res) => {
+  res.send('Hello world from contact');
+});
+
+app.get('/signin', (req, res) => {
+  res.send('Hello world from signin');
+});
+
+app.get('/signup', (req, res) => {
+  res.send('Hello world from signup');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
---
+++ Server/db/conn.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const DB = process.env.DATABASE;
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Connection Successfull.......!');
+  })
+  .catch((err) => {
+    console.log(err);
+  });

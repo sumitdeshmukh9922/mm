b7a5a5d93aa8797478276517957a277e4e3b0d91
sumitdeshmukh9922 <sumitdeshmukh9922@gmail.com>
2022-08-10 01:08:16 +0530

    Registeration Successfull..

--- Server/router/auth.js
+++ Server/router/auth.js
@@ -1,13 +1,39 @@
 const express = require('express');
 const router = express.Router();
 
+require('../db/conn');
+const User = require('../model/userSchema');
+
 router.get('/', (req, res) => {
   res.send('Hello world from Router server');
 });
 
 router.post('/register', (req, res) => {
-  console.log(req.body);
-  res.json({ message: req.body });
+  const { name, email, phone, work, password, cpassword } = req.body;
+
+  if (!name || !email || !phone || !work || !password || !cpassword) {
+    return res.status(422).json({ error: 'Field is empty' });
+  }
+
+  User.findOne({ email: email })
+    .then((userExist) => {
+      if (userExist) {
+        return res.status(422).json({ error: 'Email is already Present' });
+      }
+
+      const user = new User({ name, email, phone, work, password, cpassword });
+      user
+        .save()
+        .then(() => {
+          res.status(201).json({ message: 'Registration Successfull.....' });
+        })
+        .catch((err) =>
+          res.status(500).json({ error: 'Failed to Register.....' })
+        );
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 module.exports = router;
